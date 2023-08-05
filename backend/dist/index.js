ac8a0eac9e3bed3e653d38a52e7c66c14712f87e
akshaychavan010101 <akshaychavan010101@gmail.com>
2023-08-05 12:54:04 +0530

    changed node version

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -12,15 +12,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
+const express_1 = __importDefault(require("express"));
 const cors = require("cors");
 const db_1 = __importDefault(require("./config/db"));
 require("dotenv").config();
 const users_routes_1 = __importDefault(require("./routes/users.routes"));
 const products_routes_1 = __importDefault(require("./routes/products.routes"));
-const app = express();
+const app = (0, express_1.default)();
 app.use(cors());
-app.use(express.json());
+app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
